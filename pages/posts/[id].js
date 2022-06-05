5afb1bc6204a7eb3d2b9400265614a7e3428adac
Abdallah <hameurlain.abdallah@gmail.com>
2022-06-05 05:58:14 +0100

    finish all up until "deployment"

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from "../../utils/posts"
+import Date from '../../components/date';
+import Head from 'next/head';
+import utilStyles from '../../styles/utils.module.css';
+
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    );
+}
+
+export function getStaticPaths() {
+    const paths = getAllPostIds();
+    console.log(paths);
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+    return {
+        props: {
+            postData,
+        },
+    };
+}
